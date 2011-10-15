a12d13a85f3c04553e8b1e4e8a000774aa43bfd3
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-10-15 17:06:23 +0100

    Adding MVVM Light.

---
+++ WorkBalance/WorkBalance/ViewModelLocator.cs
@@ -0,0 +1,48 @@
+/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:ViewModelLocatorTemplate xmlns:vm="clr-namespace:WorkBalance"
+                                   x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  
+
+  You can also use Blend to do all this with the tool's support.
+  See http://www.galasoft.ch/mvvm
+*/
+
+using GalaSoft.MvvmLight;
+using WorkBalance.ViewModel;
+
+namespace WorkBalance
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time services and viewmodels
+            ////}
+            ////else
+            ////{
+            ////    // Create run time services and view models
+            ////}
+
+            Main = new MainViewModel();
+        }
+
+        /// <summary>
+        /// Gets the Main property which defines the main viewmodel.
+        /// </summary>
+        public MainViewModel Main {get; private set;}
+    }
+}
