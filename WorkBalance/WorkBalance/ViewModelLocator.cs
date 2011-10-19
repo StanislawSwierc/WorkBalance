28bc9fc161208ddcb676770dfa5c391246fbc8dc
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-10-19 09:09:29 +0100

    Making use of design-time reposn ViewModelLocator

--- WorkBalance/WorkBalance/ViewModelLocator.cs
+++ WorkBalance/WorkBalance/ViewModelLocator.cs
@@ -19,6 +19,7 @@ using WorkBalance.Repositories;
 using System.Reflection;
 using System.Collections.Generic;
 using Microsoft.Practices.Unity;
+using GalaSoft.MvvmLight.Messaging;
 
 namespace WorkBalance
 {
@@ -42,30 +43,44 @@ namespace WorkBalance
             }
             else
             {
-                m_Container = CreateContainer();
-                var ar = m_Container.Resolve<IActivityRepository>();
+                // Code runs "for real"
             }
-
-            Main = new MainViewModel(null);
+            m_Container = CreateContainer();
         }
 
         private IUnityContainer CreateContainer()
         {
             var container = new UnityContainer();
-            container.RegisterInstance<Db4objects.Db4o.IObjectContainer>(Db4objects.Db4o.Db4oFactory.OpenFile(c_Storage));
-            container.RegisterType<IActivityRepository, Repositories.Db4o.ActivityRepository>();
-            container.RegisterType<IActivityTagRepository, Repositories.Db4o.ActivityTagRepository>();
-            container.RegisterType<IInterruptionRepository, Repositories.Db4o.InterruptionRepository>();
-            container.RegisterType<IInterruptionRecordRepository, Repositories.Db4o.InterruptionRecordRepository>();
-            container.RegisterType<IInterruptionTagRepository, Repositories.Db4o.InterruptionTagRepository>();
-            container.RegisterType<ISprintRepository, Repositories.Db4o.SprintRepository>();
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                // Register repositories
+                container.RegisterType<IActivityRepository, Repositories.Design.ActivityRepository>();
+                container.RegisterType<IActivityTagRepository, Repositories.Design.ActivityTagRepository>();
+                container.RegisterType<IInterruptionRepository, Repositories.Design.InterruptionRepository>();
+                container.RegisterType<IInterruptionRecordRepository, Repositories.Design.InterruptionRecordRepository>();
+                container.RegisterType<IInterruptionTagRepository, Repositories.Design.InterruptionTagRepository>();
+                container.RegisterType<ISprintRepository, Repositories.Design.SprintRepository>();
+            }
+            else
+            {
+                // Register repositories
+                container.RegisterInstance<Db4objects.Db4o.IObjectContainer>(Db4objects.Db4o.Db4oFactory.OpenFile(c_Storage));
+                container.RegisterType<IActivityRepository, Repositories.Db4o.ActivityRepository>();
+                container.RegisterType<IActivityTagRepository, Repositories.Db4o.ActivityTagRepository>();
+                container.RegisterType<IInterruptionRepository, Repositories.Db4o.InterruptionRepository>();
+                container.RegisterType<IInterruptionRecordRepository, Repositories.Db4o.InterruptionRecordRepository>();
+                container.RegisterType<IInterruptionTagRepository, Repositories.Db4o.InterruptionTagRepository>();
+                container.RegisterType<ISprintRepository, Repositories.Db4o.SprintRepository>();
+            }
+            
+            // Register messenger
+            container.RegisterInstance<IMessenger>(new Messenger());
+
+            // Define the lifetime of the ViewModels
+            container.RegisterType<MainViewModel>(new ContainerControlledLifetimeManager());
+            
             return container;
         }
-
-        /// <summary>
-        /// Gets the Main property which defines the main viewmodel.
-        /// </summary>
-        public MainViewModel Main {get; private set;}
 
         public void Dispose()
         {
@@ -74,5 +89,10 @@ namespace WorkBalance
                 m_Container.Dispose();
             }
         }
+
+        /// <summary>
+        /// Gets the Main property which defines the main viewmodel.
+        /// </summary>
+        public MainViewModel Main { get { return m_Container.Resolve<MainViewModel>(); } }
     }
 }
