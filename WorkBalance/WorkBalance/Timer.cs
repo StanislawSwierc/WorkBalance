e0bf39016b0dd081d1622699b6a74fbbbc85913e
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-10-19 08:18:08 +0100

    Extracting timer related logic to the Timer class.

---
+++ WorkBalance/WorkBalance/Timer.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight;
+using System.Windows.Threading;
+
+namespace WorkBalance
+{
+    public enum TimerState
+    {
+        Ready,
+        Sprint,
+        Break,
+        BreakOverrun
+    }
+
+    public class Timer : ViewModelBase
+    {
+        DispatcherTimer m_Timer;
+        TimeSpan m_SprintDuration;
+        TimeSpan m_BreakDuration;
+        ITimerState m_InternalState;
+        Dictionary<TimerState, ITimerState> m_InternalStates;
+
+        public Timer()
+        {
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                m_SprintDuration = TimeSpan.FromSeconds(10);
+                m_BreakDuration = TimeSpan.FromSeconds(5);
+            }
+            else
+            {
+                m_SprintDuration = TimeSpan.FromMinutes(25);
+                m_BreakDuration = TimeSpan.FromMinutes(5);
+            }
+
+            m_InternalStates = new Dictionary<TimerState, ITimerState>();
+            m_InternalStates.Add(TimerState.Ready, new ReadyTimerState(this));
+            m_InternalStates.Add(TimerState.Sprint, new SprintTimerState(this));
+            m_InternalStates.Add(TimerState.Break, new BreakTimerState(this));
+            m_InternalStates.Add(TimerState.BreakOverrun, new BreakOverrunTimerState(this));
+
+            m_State = TimerState.Break;
+            State = TimerState.Ready;
+
+            m_Timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
+            m_Timer.Tick += HandleTick;
+            m_Timer.Start();
+        }
+
+        private TimeSpan m_Time;
+        public TimeSpan Time
+        {
+            get { return m_Time; }
+            private set
+            {
+                if (value != m_Time)
+                {
+                    m_Time = value;
+                    RaisePropertyChanged("Time");
+                }
+            }
+        }
+
+        private TimerState m_State;
+        public TimerState State
+        {
+            get { return m_State; }
+            private set
+            {
+                if (m_State != value)
+                {
+                    m_State = value;
+                    m_InternalState = m_InternalStates[m_State];
+                    m_InternalState.Activate();
+                    RaisePropertyChanged("State");
+                }
+            }
+        }
+
+        private void HandleTick(object sender, EventArgs e)
+        {
+            m_InternalState.HandleSecondElapsed();
+        }
+
+        public void ToggleTimer()
+        {
+            m_InternalState.ToggleTimer();
+        }
+
+        #region Internal Types
+
+        internal interface ITimerState
+        {
+            void Activate();
+            void HandleSecondElapsed();
+            void ToggleTimer();
+        }
+
+        internal abstract class TimerStateBase : ITimerState
+        {
+            protected Timer m_Timer;
+
+            public TimerStateBase(Timer timer)
+            {
+                m_Timer = timer;
+            }
+
+            public virtual  void Activate()
+            {
+            }
+
+            public virtual void HandleSecondElapsed()
+            {
+            }
+
+            public virtual void ToggleTimer()
+            {
+            }
+        }
+
+        internal class ReadyTimerState : TimerStateBase
+        {
+            public ReadyTimerState(Timer timer)
+                : base(timer)
+            {
+            }
+
+            public override void Activate()
+            {
+                m_Timer.Time = m_Timer.m_SprintDuration;
+            } 
+
+            public override void ToggleTimer()
+            {
+                m_Timer.State = TimerState.Sprint;
+            }
+        }
+
+        internal class SprintTimerState : TimerStateBase
+        {
+            public SprintTimerState(Timer timer)
+                : base(timer)
+            {
+            }
+
+            public override void HandleSecondElapsed()
+            {
+                m_Timer.Time = m_Timer.Time.Subtract(TimeSpan.FromSeconds(1));
+                if (m_Timer.Time.Ticks == 0)
+                {
+                    m_Timer.State = TimerState.Break;
+                }
+            }
+
+            public override void ToggleTimer()
+            {
+                m_Timer.State = TimerState.Ready;
+            }
+        }
+
+        internal class BreakTimerState : TimerStateBase
+        {
+            public BreakTimerState(Timer timer)
+                : base(timer)
+            {
+            }
+
+            public override void HandleSecondElapsed()
+            {
+                m_Timer.Time = m_Timer.Time.Add(TimeSpan.FromSeconds(1));
+                if (m_Timer.Time > m_Timer.m_BreakDuration)
+                {
+                    m_Timer.State = TimerState.BreakOverrun;
+                }
+            }
+
+            public override void ToggleTimer()
+            {
+                m_Timer.State = TimerState.Ready;
+            }
+        }
+
+        internal class BreakOverrunTimerState : TimerStateBase
+        {
+            public BreakOverrunTimerState(Timer timer)
+                : base(timer)
+            {
+            }
+
+            public override void HandleSecondElapsed()
+            {
+                m_Timer.Time = m_Timer.Time.Add(TimeSpan.FromSeconds(1));
+            }
+
+            public override void ToggleTimer()
+            {
+                m_Timer.State = TimerState.Ready;
+            }
+        }
+
+        #endregion
+    }
+}
--- WorkBalance/WorkBalance/ViewModel/MainViewModel.cs
+++ WorkBalance/WorkBalance/ViewModel/MainViewModel.cs
@@ -5,15 +5,11 @@ using GalaSoft.MvvmLight.Command;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Messaging;
 using System.Diagnostics.Contracts;
+using System.ComponentModel;
 
 namespace WorkBalance.ViewModel
 {
-    public enum TimerState
-    {
-        Ready,
-        Running,
-        Break
-    }
+
 
     /// <summary>
     /// This class contains properties that the main View can data bind to.
@@ -29,14 +25,7 @@ namespace WorkBalance.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        //private static readonly TimeSpan c_Interval = TimeSpan.FromMinutes(25);
-        private static readonly TimeSpan c_Interval = TimeSpan.FromSeconds(10);
-        private static readonly string c_StartTimerText = "Start Timer";
-        private static readonly string c_StopTimerText = "Stop Timer";
-        private static readonly string c_StopBreakText = "Stop Break";
-
-        DispatcherTimer m_Timer;
-        IMessenger m_Messenger; 
+        IMessenger m_Messenger;
 
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
@@ -54,113 +43,50 @@ namespace WorkBalance.ViewModel
             ////{
             ////    // Code runs "for real"
             ////}
-            m_Clock = c_Interval;
-
-            m_Timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
-            m_Timer.Tick += HandleTick;
-            m_ToggleTimerActionName = c_StartTimerText;
-            State = TimerState.Ready;
-            m_ToggleTimerCommand = new RelayCommand(ToggleCommand);
+            Timer = new WorkBalance.Timer();
+            // Translate state change notification and propagate it to the user interface
+            Timer.PropertyChanged += new PropertyChangedEventHandler(
+                CreatePropertyChangedHandler("State", s => RaisePropertyChanged("ToggleTimerActionName")));
+            m_ToggleTimerCommand = new RelayCommand(Timer.ToggleTimer);
             m_CloseCommand = new RelayCommand(() => App.Current.Shutdown());            
         }
 
-        private void ToggleCommand()
-        {
-            switch (State)
-            {
-                case TimerState.Ready:
-                    State = TimerState.Running;
-                    break;
-                case TimerState.Running:
-                    State = TimerState.Ready;
-                    break;
-                case TimerState.Break:
-                    State = TimerState.Ready;
-                    break;
-                default:
-                    break;
-            }
-        }
+        public Timer Timer { get; private set; }
 
-        void HandleTick(object sender, EventArgs e)
-        {
-            switch (State)
-            {
-                case TimerState.Ready:
-                    break;
-                case TimerState.Running:
-                    Clock = Clock.Subtract(TimeSpan.FromSeconds(1));
-                    break;
-                case TimerState.Break:
-                    Clock = Clock.Add(TimeSpan.FromSeconds(1));
-                    break;
-                default:
-                    break;
-            }
-            if (State == TimerState.Running && Clock.Ticks == 0) State = TimerState.Break;
-            if (State == TimerState.Break && Clock == c_Interval) State = TimerState.Ready;
-        }
-
-
-        private TimeSpan m_Clock;
-        public TimeSpan Clock 
-        { 
-            get { return m_Clock; }
-            private set 
-            {
-                if (value != m_Clock)
-                {
-                    m_Clock = value;
-                    RaisePropertyChanged("Clock");
-                }
-            }
-        }
-
-        private TimerState m_State;
-        public TimerState State 
-        { 
-            get { return m_State; }
-            private set
-            {
-                if (m_State != value)
-                {
-                    m_State = value;
-                    switch (m_State)
-                    {
-                        case TimerState.Ready:
-                            m_Timer.IsEnabled = false;
-                            Clock = c_Interval;
-                            ToggleTimerActionName = c_StartTimerText;
-                            break;
-                        case TimerState.Running:
-                            m_Timer.IsEnabled = true;
-                            ToggleTimerActionName = c_StopTimerText;
-                            break;
-                        case TimerState.Break:
-                            ToggleTimerActionName = c_StopBreakText;
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-        }
-
-
-        private string m_ToggleTimerActionName;
         public string ToggleTimerActionName
         {
-            get { return m_ToggleTimerActionName; }
-            private set
+            get 
             {
-                if (m_ToggleTimerActionName != value)
+                string result = null;
+                switch (Timer.State)
                 {
-                    m_ToggleTimerActionName = value;
-                    RaisePropertyChanged("ToggleTimerActionName");
+                    case TimerState.Ready:
+                        result = "Start Sprint";
+                        break;
+                    case TimerState.Sprint:
+                        result = "Abort Sprint";
+                        break;
+                    case TimerState.Break:
+                        result = "Abort Break";
+                        break;
+                    case TimerState.BreakOverrun:
+                        result = "Stop Break";
+                        break;
                 }
+                return result;
             }
         }
-        
+
+        private Action<object, PropertyChangedEventArgs> CreatePropertyChangedHandler(string property, Action<object> handler)
+        {
+            return new Action<object,PropertyChangedEventArgs>(delegate(object sender, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName == "State")
+                {
+                    handler(sender);
+                }
+            });
+        }
 
         private RelayCommand m_ToggleTimerCommand;
         public ICommand ToggleTimerCommand { get { return m_ToggleTimerCommand; } }
