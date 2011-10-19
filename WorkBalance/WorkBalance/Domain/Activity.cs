abdf6385fd94d5b8dff57af7f0764684a8dccff9
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-10-19 10:59:04 +0100

    Adding Active flag to the Activity together with some comments.

--- WorkBalance/WorkBalance/Domain/Activity.cs
+++ WorkBalance/WorkBalance/Domain/Activity.cs
@@ -7,11 +7,40 @@ namespace WorkBalance.Domain
 {
     public class Activity
     {
+        /// <summary>
+        /// The name of the Activity
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// Expected effort measured in sprints
+        /// </summary>
         public int ExpectedEffort { get; set; }
+
+        /// <summary>
+        /// Actual effort measured in sprints
+        /// </summary>
         public int ActualEffort { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value indicating wheather the Activity should 
+        /// be listed in the ActivityInventory
+        /// </summary>
+        public bool Active { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value indicating wheather the Activity is completed
+        /// </summary>
         public bool Completed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time when the Activity was created
+        /// </summary>
         public DateTime CreationTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of associated tags
+        /// </summary>
         public ICollection<ActivityTag> Tags { get; set; }
     }
 }
