b4a350e4651774f1d078e31a4f028b5d099c744a
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-10-19 09:08:19 +0100

    Ading design-time repositories

---
+++ WorkBalance/WorkBalance/Repositories/Design/DesignRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace WorkBalance.Repositories.Design
+{
+    public class DesignRepository<TEntity> : IRepository<TEntity>
+    {
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> expression)
+        {
+            return Enumerable.Empty<TEntity>();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Enumerable.Empty<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+        }
+
+        public void Delete(TEntity entity)
+        {
+        }
+
+        public void Update(TEntity entity)
+        {
+        }
+    }
+}
---
+++ WorkBalance/WorkBalance/Repositories/Design/Repositories.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WorkBalance.Domain;
+
+namespace WorkBalance.Repositories.Design
+{
+    public class ActivityRepository : DesignRepository<Activity>, IActivityRepository
+    {
+    }
+
+    public class ActivityTagRepository : DesignRepository<ActivityTag>, IActivityTagRepository
+    {
+    }
+
+    public class InterruptionRepository : DesignRepository<Interruption>, IInterruptionRepository
+    {
+    }
+
+    public class InterruptionRecordRepository : DesignRepository<InterruptionRecord>, IInterruptionRecordRepository
+    {
+    }
+
+    public class InterruptionTagRepository : DesignRepository<InterruptionTag>, IInterruptionTagRepository
+    {
+    }
+
+    public class SprintRepository : DesignRepository<Sprint>, ISprintRepository
+    {
+    }
+}
