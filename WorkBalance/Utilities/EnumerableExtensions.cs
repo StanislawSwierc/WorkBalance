33a008d9cd640b13efec983954788479d35df1ad
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-11-03 08:43:28 +0000

    Adding ForEach extension to IEnumerable<T>.

---
+++ WorkBalance/Utilities/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WorkBalance.Utilities
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable)
+            {
+                action(item);
+            }
+        }
+    }
+}
