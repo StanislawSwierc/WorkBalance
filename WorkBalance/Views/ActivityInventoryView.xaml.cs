30e414f6863304b29c7a6b0a174cf96f4873ee87
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-11-05 20:08:57 +0000

    Ranameing _vm to _ViewModel.

--- WorkBalance/Views/ActivityInventoryView.xaml.cs
+++ WorkBalance/Views/ActivityInventoryView.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Shapes;
 using System.ComponentModel;
 using WorkBalance.ViewModel;
 using WorkBalance.Domain;
+using System.ComponentModel.Composition;
+using ReactiveUI;
+using System.Reactive.Linq;
+using System.Reactive;
 
 namespace WorkBalance
 {
@@ -21,8 +25,8 @@ namespace WorkBalance
     /// Interaction logic for ActivityInventoryView.xaml
     /// </summary>
     public partial class ActivityInventoryView : UserControl
-    {
-        private ActivityInventoryViewModel _vm;
+    {    
+        private ActivityInventoryViewModel _ViewModel;
 
         public ActivityInventoryView()
         {
@@ -32,22 +36,21 @@ namespace WorkBalance
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-             if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
-             {
-                 _vm = (ActivityInventoryViewModel)this.DataContext;
-             }
+            if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this) && _ViewModel == null)
+            {
+                _ViewModel = (ActivityInventoryViewModel)this.DataContext;
+            }
         }
 
         private void Button_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            _vm.SelectActivityCommand.Execute(null);
+            _ViewModel.SelectActivityCommand.Execute(null);
         }
 
         private void activitiesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             // It would be better if two lists were compare against each other but that should also work.
-            _vm.SelectedActivities = activitiesListBox.SelectedItems.Cast<Activity>().ToList();
+            _ViewModel.SelectedActivities = activitiesListBox.SelectedItems.Cast<Activity>().ToList();
         }
-
     }
 }
