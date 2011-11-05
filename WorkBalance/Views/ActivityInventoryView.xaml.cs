1b971322f7870a586cc1827cab0834f01ec5f027
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-11-05 14:03:38 +0000

    Moving a cast to ActivityInventoryViewModel in ActivityInventoryView code behind to not design-time block.

--- WorkBalance/Views/ActivityInventoryView.xaml.cs
+++ WorkBalance/Views/ActivityInventoryView.xaml.cs
@@ -32,14 +32,10 @@ namespace WorkBalance
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            _vm = (ActivityInventoryViewModel)this.DataContext;
-            // Do not load your data at design time.
-            // if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
-            // {
-            // 	//Load your data here and assign the result to the CollectionViewSource.
-            // 	System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["Resource Key for CollectionViewSource"];
-            // 	myCollectionViewSource.Source = your data
-            // }
+             if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+             {
+                 _vm = (ActivityInventoryViewModel)this.DataContext;
+             }
         }
 
         private void Button_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
