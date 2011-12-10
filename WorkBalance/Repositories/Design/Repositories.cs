04484842748cacb19afb37f4d6db89dc561c4276
StanislawSwierc <stanislaw.swierc@gmail.com>
2011-12-10 11:34:37 +0000

    Make WorkBalance.Repositories.Design.ActivityRepository use SprintRepository to create Activities with som esprints added.

--- WorkBalance/Repositories/Design/Repositories.cs
+++ WorkBalance/Repositories/Design/Repositories.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using WorkBalance.Domain;
 using WorkBalance.Utilities;
+using System.ComponentModel.Composition;
 
 namespace WorkBalance.Repositories.Design
 {
     [DesignTimeExport(typeof(IActivityRepository), DesignTime = true)]
     public class ActivityRepository : DesignRepository<Activity>, IActivityRepository
     {
+        [Import(typeof(ISprintRepository))]
+        ISprintRepository SprintRepository { get; set; }
+
         public IEnumerable<Activity> GetActive()
         {
             return CreateInstances();
@@ -17,7 +21,7 @@ namespace WorkBalance.Repositories.Design
 
         protected override Activity CreateInstance()
         {
-            return new Activity()
+            var result = new Activity()
             {
                 Archived = true,
                 ActualEffort = 3,
@@ -26,6 +30,8 @@ namespace WorkBalance.Repositories.Design
                 ExpectedEffort = 5,
                 Name = "Sample Activity",
             };
+            result.Sprints = SprintRepository.GetAll().Select(s => { s.Activity = result; return s; }).ToList();
+            return result;
         }
     }
 
@@ -70,7 +76,7 @@ namespace WorkBalance.Repositories.Design
     {
         protected override Sprint CreateInstance()
         {
-            throw new NotImplementedException();
+            return new Sprint();
         }
     }
 }
