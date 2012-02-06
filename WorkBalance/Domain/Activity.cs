a228a04b4045028e052cf73d1b0dce1f25f176a4
StanislawSwierc <stanislaw.swierc@gmail.com>
2012-02-06 21:55:55 +0000

    Remove PostSharp

--- WorkBalance/Domain/Activity.cs
+++ WorkBalance/Domain/Activity.cs
@@ -2,53 +2,93 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using WorkBalance.Aspects;
 using System.ComponentModel;
+using ReactiveUI;
+using ReactiveUI.Xaml;
 
 namespace WorkBalance.Domain
 {
-    [NotifyPropertyChanged]
-    public class Activity : INotifyPropertyChanged
+    public class Activity : ReactiveObject
     {
         /// <summary>
         /// The name of the Activity
         /// </summary>
-        public string Name { get; set; }
-        
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { this.RaiseAndSetIfChanged(self => self.Name, value); }
+        }
+
         /// <summary>
         /// Expected effort measured in sprints
         /// </summary>
-        public int ExpectedEffort { get; set; }
+        private int _ExpectedEffort;
+        public int ExpectedEffort
+        {
+            get { return _ExpectedEffort; }
+            set { this.RaiseAndSetIfChanged(self => self.ExpectedEffort, value); }
+        }
 
         /// <summary>
         /// Actual effort measured in sprints
         /// </summary>
-        public int ActualEffort { get; set; }
+        private int _ActualEffort;
+        public int ActualEffort
+        {
+            get { return _ActualEffort; }
+            set { this.RaiseAndSetIfChanged(self => self.ActualEffort, value); }
+        }
 
         /// <summary>
         /// Gets or sets the value indicating wheather the Activity is archived
         /// </summary>
-        public bool Archived { get; set; }
+        private bool _Archived;
+        public bool Archived
+        {
+            get { return _Archived; }
+            set { this.RaiseAndSetIfChanged(self => self.Archived, value); }
+        }
 
         /// <summary>
         /// Gets or sets the value indicating wheather the Activity is completed
         /// </summary>
-        public bool Completed { get; set; }
+        private bool _Completed;
+        public bool Completed
+        {
+            get { return _Completed; }
+            set { this.RaiseAndSetIfChanged(self => self.Completed, value); }
+        }
 
         /// <summary>
         /// Gets or sets the time when the Activity was created
         /// </summary>
-        public DateTime CreationTime { get; set; }
+        private DateTime _CreationTime;
+        public DateTime CreationTime
+        {
+            get { return _CreationTime; }
+            set { this.RaiseAndSetIfChanged(self => self.CreationTime, value); }
+        }
 
         /// <summary>
         /// Gets or sets the collection of associated tags
         /// </summary>
-        public ICollection<ActivityTag> Tags { get; set; }
+        private ICollection<ActivityTag> _Tags;
+        public ICollection<ActivityTag> Tags
+        {
+            get { return _Tags; }
+            set { this.RaiseAndSetIfChanged(self => self.Tags, value); }
+        }
 
         /// <summary>
         /// Gets of sets the collection of sprints
         /// </summary>
-        public ICollection<Sprint> Sprints { get; set; }
+        private ICollection<Sprint> _Sprints;
+        public ICollection<Sprint> Sprints
+        {
+            get { return _Sprints; }
+            set { this.RaiseAndSetIfChanged(self => self.Sprints, value); }
+        }
 
         public Activity()
         {
@@ -56,7 +96,5 @@ namespace WorkBalance.Domain
             Sprints = new List<Sprint>();
             CreationTime = DateTime.Now;
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
